10e6cdec1751fbad339fd021849c80949d8ed97c
Mikhail <me@reze.moe>
2025-12-05 06:01:33 +0500

    feat: implement dashboard login page

---
+++ app/pages/dashboard/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>Logged in as {{ auth.session?.username }}</p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { useAuthStore } from '~/stores/auth'
+
+definePageMeta({
+  layout: 'dashboard'
+})
+useHead({ title: "dashboard" });
+
+const auth = useAuthStore()
+</script>
+
+<style lang="scss" scoped>
+
+</style>
