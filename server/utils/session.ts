7c07e8edacef061559f981fda31b3ec209ddd30f
Mikhail <me@reze.moe>
2025-12-05 06:00:21 +0500

    fix(api): kv storage was not working because of incorrect function usage

--- server/utils/session.ts
+++ server/utils/session.ts
@@ -1,4 +1,3 @@
-// server/utils/session.ts
 import bcrypt from 'bcrypt'
 import { randomUUID } from 'crypto'
 import { useStorage } from '#imports'
@@ -9,21 +8,17 @@ export interface SessionData {
   access_level: number
 }
 
-const storage = useStorage('sessions')
+const storage = useStorage()
 
 export async function createSession(user: SessionData): Promise<string> {
   const salt = await bcrypt.genSalt(5);
   const token = await bcrypt.hash(`${user.username}__${randomUUID()}`, salt);
-  await storage.set(token, user, { ttl: 60 * 60 * 24 });
+  await storage.setItem(`session:${token}`, user, { ttl: 60 * 60 * 24 });
 
   return token;
 }
 
 export async function getSession(token: string): Promise<SessionData | null> {
-  const session = await storage.get<SessionData>(token)
+  const session = await storage.getItem<SessionData>(`session:${token}`)
   return session || null
-}
-
-export async function destroySession(token: string): Promise<void> {
-  await storage.delete(token)
 }
